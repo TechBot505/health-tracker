a6ce3ed747bce8366db166cb7fcc51cf4079a3b4
Rohit <rohitdhanotia2003@gmail.com>
2024-08-11 12:35:10 +0530

    Added Navbar and Footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './components/form/form.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'form',
+    pathMatch: 'full'
+  },
+  {
+    path: 'form',
+    component: FormComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
